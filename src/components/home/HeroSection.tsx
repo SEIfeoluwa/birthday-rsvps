33a36019f50f49c7d4694ca5fd7c729cac3912dd
Ife Adebisi <adebisiife@gmail.com>
2026-07-16 22:54:35 -0400

    Content and size finalization

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function Hero({ onRsvpClick }: HeroProps) {
       <div className="hero-content">
         <p className="hero-eyebrow">Please join us in celebrating</p>
         <h1>Mr &amp; Mrs Fayemi</h1>
-        <p className="hero-ages">A celebration of 75 &amp; 70</p>
+        <p className="hero-ages">Celebration of 75 &amp; 70 years of life</p>
         <p className="hero-details">
           Saturday, September 19th, 2026 at 3:00 PM<br />
           Richland Golf Club, 50 Glenbrook Dr, Middletown, MD 21769
